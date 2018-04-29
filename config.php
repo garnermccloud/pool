cc69303f434ce6ab375bf1b71d01c968d4970a91
Garner Fox McCloud <garner@datadoghq.com>
2018-04-29 12:46:24 -0400

    Update config.php

--- config.php
+++ config.php
@@ -3,16 +3,16 @@ $pool_config['db_connect']="mysql:host=".getenv('POOL_MYSQL_HOST').";dbname=".ge
 $pool_config['db_user']=getenv('MYSQL_USER');
 $pool_config['db_pass']=getenv('MYSQL_PASSWORD');
 
-$pool_config['max_deadline']=1000000; 
-$pool_config['node_path']="/var/www/node";  // path to where the node is installed
-$pool_config['node_url']="http://127.0.0.1:30000"; // Node's access url
+$pool_config['max_deadline']=getenv('MAX_DEADLINE'); 
+$pool_config['node_path']=getenv('NODE_PATH');  // path to where the node is installed
+$pool_config['node_url']=getenv('NODE_URL');; // Node's access url
 $pool_config['public_key']=getenv('POOL_PUBLIC_KEY');
 $pool_config['private_key']=getenv('POOL_PRIVATE_KEY');
 $pool_config['address']=getenv('POOL_ADDRESS');
 $pool_config['fee_address']=getenv('FEE_ADDRESS');
-$pool_config['fee']=0.02;   // 0.02 = 2%  |   pool fee
-$pool_config['historic_reward']=1; // percentage going to historic shares 1=100%, 0.3=30%, 0=none
-$pool_config['current_reward']=0; // percentage going to current shares1=100%, 0.3=30%, 0=none
-$pool_config['miner_reward']=0; // percentage going to block winner 1=100%, 0.3=30%, 0=none
-$pool_config['min_payout']=3;
+$pool_config['fee']=getenv('FEE');;   // 0.02 = 2%  |   pool fee
+$pool_config['historic_reward']=getenv('HISTORIC_REWARD');; // percentage going to historic shares 1=100%, 0.3=30%, 0=none
+$pool_config['current_reward']=getenv('CURRENT_REWARD');; // percentage going to current shares1=100%, 0.3=30%, 0=none
+$pool_config['miner_reward']=getenv('MINER_REWARD');; // percentage going to block winner 1=100%, 0.3=30%, 0=none
+$pool_config['min_payout']=getenv('MIN_PAYOUT');;
 ?>
