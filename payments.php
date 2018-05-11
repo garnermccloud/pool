efd4c1345ea8a47e400241ccb69805b53ca042a5
Garner Fox McCloud <garner@datadoghq.com>
2018-05-11 08:19:54 -0400

    Update payments.php

--- payments.php
+++ payments.php
@@ -26,6 +26,8 @@ register_shutdown_function("shut_down");
 set_time_limit(0);
 if(php_sapi_name() !== 'cli') die("This should only be run as cli");
 
+$date = date('Format String', time());
+
 require_once("db.php");
 
 function pay_post($url, $data=array()){
@@ -65,7 +67,7 @@ if($hour==10&&$min<20) $blocks_paid=5000;
 
 echo "\n----------------------------------------------------------------------------------\n";
 $current=$aro->single("SELECT height FROM blocks ORDER by height DESC LIMIT 1");
-echo "Current block $current\n";
+echo "$date: Current block $current\n";
 
 
 $db->run("DELETE FROM miners WHERE historic+shares<=20");
@@ -75,19 +77,19 @@ $db->run("UPDATE miners SET pending=(SELECT SUM(val) FROM payments WHERE done=0 
 
 
 $r=$db->run("SELECT DISTINCT block FROM payments WHERE height<:h AND done=0 AND height>=:h2",array(":h"=>$current-10, ":h2"=>$current-$blocks_paid));
-if(count($r)==0) die("No payments pending\n");
+if(count($r)==0) die("$date: No payments pending\n");
 
 $db->run("DELETE FROM miners WHERE shares=0 AND historic=0 AND updated<UNIX_TIMESTAMP()-3600");
 $db->run("DELETE FROM workers WHERE updated<UNIX_TIMESTAMP()-3600");
 
 // check for orphan blocks
 foreach($r as $x){
-	echo "Checking $x[block]\n";
+	echo "$date: Checking $x[block]\n";
         $s=$aro->single("SELECT COUNT(1) FROM blocks WHERE id=:id",array(":id"=>$x['block']));
         if($s==0){ 
 		 $db->run("DELETE FROM blocks WHERE id=:id",array(":id"=>$x['block']));
 		$db->run("DELETE FROM payments WHERE block=:id",array(":id"=>$x['block']));
-		echo "Deleted block: $x[block]\n";
+		echo "$date: Deleted block: $x[block]\n";
 	}
 }
 
@@ -105,26 +107,26 @@ foreach($r as $x){
 	$private_key=$pool_config['private_key'];
 
 	$res=pay_post("/api.php?q=send",array("dst"=>$x['address'],"val"=>$val, "private_key"=>$private_key, "public_key"=>$public_key, "version"=>1));
-	echo "$val\n";
-	echo "$x[address]\n";
-	if($res['status']!="ok") print("ERROR: Status: $res[status], $res[data]\n");
+	echo "$date: $val\n";
+	echo "$date: $x[address]\n";
+	if($res['status']!="ok") print("$date: ERROR: Status: $res[status], $res[data]\n");
 	else{
 		$total_paid+=$x['v'];
 		
-		 echo "Transaction sent - $x[address] - $val! Transaction id: $res[data]\n";
+		 echo "$date: Transaction sent - $x[address] - $val! Transaction id: $res[data]\n";
 		$db->run("UPDATE payments SET txn=:txn, done=1 WHERE address=:address AND height<:h AND done=0 AND height>=:h2",array(":h"=>$current-10, ":h2"=>$current-$blocks_paid, ":txn"=>$res['data'],":address"=>$x['address']));
 		$db->run("UPDATE miners  SET total_paid=total_paid + :h WHERE id=:p",array(":h"=>$x['v'], ":p"=>$x['address']));
-		echo "DB updated\n";
+		echo "$date: DB updated\n";
 	}
 }
 
 $old=$db->single("SELECT val FROM info WHERE id='total_paid'");
 $new=$old+$total_paid;
-echo "Total paid: $new\n";
+echo "$date: Total paid: $new\n";
 
 $db->run("UPDATE info SET val=:s WHERE id='total_paid'",array(":s"=>$new));
 $not=$db->single("SELECT SUM(val) FROM payments WHERE done=0");
-echo "Pending balance: $not\n";
+echo "$date: Pending balance: $not\n";
 
 
 
